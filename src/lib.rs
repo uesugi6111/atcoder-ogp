1672afca7f45b319cea7e35da1c47e75b0b03a4f
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2021-06-10 21:44:21 +0000

    impl main

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,39 @@
+mod crawl;
+mod template;
+
+use crawl::crawl;
 use serde::Serialize;
-use serde_json::Value;
+use template::{card, TEMPLETES};
 
 use lambda_runtime::Context;
 use std::collections::HashMap;
 
 #[derive(Debug, Serialize)]
-pub struct Output(pub HashMap<String, Value>);
+pub struct Output {
+    #[serde(rename = "statusCode")]
+    status_code: u64,
+    #[serde(rename = "Content-Type")]
+    content_type: String,
+    body: String,
+}
+
+impl Output {
+    pub fn new(body: &str) -> Self {
+        Output {
+            status_code: 200,
+            content_type: "text/html".to_string(),
+            body: body.to_string(),
+        }
+    }
+}
 
 pub async fn run(
     _event: HashMap<String, String>,
     _context: Context,
 ) -> Result<Output, Box<dyn std::error::Error>> {
-    todo!()
+    let param = "url";
+    let target_page = crawl(param).await?;
+
+    let body = card(&TEMPLETES, target_page);
+    Ok(Output::new(&body))
 }
